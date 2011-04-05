b1b90a3d60db10fb2251581b14e36dbad85a5b27
keke <iamkeke@gmail.com>
2011-04-05 15:04:51 +0800

    Added i18n support

--- src/main/scala/net/luerything/web/page/Home.scala
+++ src/main/scala/net/luerything/web/page/Home.scala
@@ -1,17 +1,19 @@
 package net.luerything.web.page
 
 import org.apache.velocity.app.VelocityEngine
-import org.restlet.{Request, Application}
+import org.restlet.Request
+import org.springframework.context.MessageSource
+import java.util.Locale
 
 /**
  *
  */
 
-class Home(val pageName: String, val ve: VelocityEngine) extends Page with DefaultModel {
-
-}
-
-object DefaultModel {
+class Home(val pageName: String, val ve: VelocityEngine, messageSource: MessageSource) extends Page with DefaultModel {
+  override def model = {
+    val m = super.model
+    m ++ Array("bodyPart" -> "_homebody.vt", "title" -> messageSource.getMessage("home.title", null, Locale.ENGLISH))
+  }
 }
 
 trait DefaultModel extends PageSupport {
