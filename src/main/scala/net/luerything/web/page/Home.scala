b4fd47cf0da8558fe07e684cbd5f36a19552733a
keke <iamkeke@gmail.com>
2011-04-06 22:01:29 +0800

    simple search bar

--- src/main/scala/net/luerything/web/page/Home.scala
+++ src/main/scala/net/luerything/web/page/Home.scala
@@ -9,15 +9,18 @@ import java.util.Locale
  *
  */
 
-class Home(val pageName: String, val ve: VelocityEngine, messageSource: MessageSource) extends Page with DefaultModel {
+class Home(val pageName: String, val ve: VelocityEngine, val messageSource: MessageSource) extends Page with CommonModel {
   override def model = {
     val m = super.model
-    m ++ Array("bodyPart" -> "_homebody.vt", "title" -> messageSource.getMessage("home.title", null, Locale.ENGLISH))
+    m ++ Array("bodyPart" -> "_homebody.vt", "headPart" -> "_homehead.vt", "locale" -> Locale.ENGLISH)
   }
 }
 
-trait DefaultModel extends PageSupport {
+trait CommonModel extends PageSupport {
+  val messageSource: MessageSource
+
   override abstract def model: Map[String, Object] = {
-    Map("ctxRoot" -> Request.getCurrent.getRootRef.getParentRef.toString)
+    Map("ctxRoot" -> Request.getCurrent.getRootRef.getParentRef.toString,
+      "messageSource" -> messageSource)
   }
 }
