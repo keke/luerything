d68d3e4e42330b7817b3072f835628ce6895ef94
keke <iamkeke@gmail.com>
2014-02-06 14:05:41 +0800

    master directive: add search box as a directive

--- platform/ui/app/scripts/masthead.js
+++ platform/ui/app/scripts/masthead.js
@@ -6,10 +6,15 @@
 
   define(['app'], function (ltApp) {
     ltApp.directive('ltMasthead', ['templateRoot', function (tRoot) {
-      return {
-        replace: true,
-        templateUrl: tRoot + '/masthead.html'
-      }
-    }]);
+          return {
+            replace: true,
+            templateUrl: tRoot + '/masthead.html'
+          }
+        }]).directive('ltSearchBox', ['templateRoot', function (tRoot) {
+          return {
+            replace: true,
+            templateUrl: tRoot + '/searchbox.html'
+          }
+        }]);
   });
 })(define);
