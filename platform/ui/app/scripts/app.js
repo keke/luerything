3c84bf0a7aace210ab8f01cbc633283699f586c7
keke <iamkeke@gmail.com>
2014-02-06 14:06:22 +0800

    remove resize handler from element when it is destroyed

--- platform/ui/app/scripts/app.js
+++ platform/ui/app/scripts/app.js
@@ -1,20 +1,24 @@
 (function (define) {
   'use strict';
 
-  define(['angular', 'jquery', 'ngRoute', 'ltShared/app-config'], function (angular, $) {
-    return angular.module('ltApp', ['ngRoute']).directive('ltHeightMon', ['$window', '$timeout', function ($window, $timeout) {
-      return{
-        replace: false,
-        link: function postLink($scope, elem, attrs) {
-          var wnd = $($window);
-          var sel = attrs.ltHeightMon;
-          var resize = function () {
-            elem.height(wnd.height() - $(sel).height());
-          };
-          wnd.on('resize', resize);
-          $timeout(resize, 100);
-        }
-      }
-    }]);
+  define(['angular', 'jquery', 'ngRoute'], function (angular, $) {
+    return angular.module('ltApp', ['ngRoute'])
+        .directive('ltHeightMon', ['$window', '$timeout', function ($window, $timeout) {
+          return{
+            replace: false,
+            link: function postLink($scope, elem, attrs) {
+              var wnd = $($window);
+              var sel = attrs.ltHeightMon;
+              var resize = function () {
+                elem.height(wnd.height() - $(sel).height());
+              };
+              wnd.on('resize', resize);
+              $timeout(resize, 100);
+              elem.on('$destroy', function () {
+                wnd.off('resize', resize);
+              });
+            }
+          }
+        }]);
   });
 })(define);
