535f0c3f5d5cf6b2c5e07746816bdc0f9bc08f37
keke <iamkeke@gmail.com>
2014-02-09 23:50:39 +0800

    add routeProvider config for root
    use resolve to make sure i18next is loaded

--- ui/app/scripts/app-config.js
+++ ui/app/scripts/app-config.js
@@ -1,16 +1,26 @@
 /**
  * @author keke
  */
-(function (define) {
+(function(define) {
   'use strict';
-  define(['app', 'masthead', 'app-nav', 'ltShared/i18next', 'ltShared/cfg'], function (ltApp) {
-    return ltApp.constant('templateRoot', 'scripts').config(['$locationProvider', 'i18nextServiceProvider',
-          function ($locationProvider, i18nextServiceProvider) {
-            $locationProvider.html5Mode(true).hashPrefix('!');
-            i18nextServiceProvider.addNamespace('lt');
-          }]).run(['i18nextService',
-          function (i18nextService) {
-            i18nextService.init();
+  define(['app', 'masthead', 'app-nav', 'ltShared/i18next', 'ltShared/cfg'], function(ltApp) {
+    return ltApp.constant('templateRoot', 'scripts').
+      config(['$locationProvider', 'i18nextServiceProvider', '$routeProvider',
+        function($locationProvider, i18nextServiceProvider, $routeProvider) {
+          $locationProvider.html5Mode(true).hashPrefix('!');
+          i18nextServiceProvider.addNamespace('lt');
+          $routeProvider.when('/', {
+            resolve:{
+              delay: ['$q','i18nextService',function($q, i18nextService){
+                return $q.all([i18nextService.init()]);
+              }]
+            }
+          }), otherwise({
+            redirectTo: '/'
+          });
+        }]).run(['i18nextService',
+          function(i18nextService) {
+        i18nextService.init();
           }]);
   });
 })(define);
